065ef298308d19bfba8c28f1e4342c363cc96eeb
twmoon <jdk19990830@gmail.com>
2025-02-07 16:47:43 +0900

    boom

--- credentials.py
+++ credentials.py
@@ -1,9 +1,9 @@
 # credentials.py
 
 # pushover api 키 (https://pushover.net/ 회원가입 필수)
-PUSHOVER_TOKEN = ""
-PUSHOVER_USER_KEY = ""
+PUSHOVER_TOKEN = "a5s7f36sxuq26kbyathwr7tji2wvcz"
+PUSHOVER_USER_KEY = "uicav46f7uma4du2qppqhssqswegxp"
 
 # 수강신청 서버 세션 토큰
-SGJSESSIONID = ""
-WMONID = ""
+SGJSESSIONID = "UEKcAQnrIZlOZ9eCxgaUejxuPCCesaRanvuzWA3LnZjIgaEBvAift1X9HFAARkxH.amV1c19kb21haW4vc3VnYW5nMg=="
+WMONID = "-iZoXxFgbwy"
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# main.py
+# main__1.py
 import time
 import random
 from sugang_request import send_sugang_request
@@ -9,12 +9,12 @@ from subjects import subject_data
 while True:
     try:
         # 모든 과목에 대해 성공하면 종료
-        if not subject_dict:
+        if not subject_data:
             print("모든 학수번호 수강신청 성공! 종료합니다.")
             break
 
         # 학수번호로 수강신청 request 전송 (신청 완료된 과목 삭제)
-        for course, div in list(subject_dict.items()):
+        for course, div in list(subject_data.items()):
             response_text = send_sugang_request(course, div)
             print(f"수강신청 요청 전송 - 학수번호: {course}, 분반: {div}")
             print(response_text)
@@ -29,13 +29,13 @@ while True:
             elif "true" in response_text:
                 send_mobile_alert(f"수강신청 성공!! (학수번호: {course}, 분반: {div})")
                 print(f"수강신청 성공으로 {course} 제거합니다.")
-                del subject_dict[course]
+                del subject_data[course]
 
             # 다음 항목 시도 전 랜덤 딜레이 (매크로 탐지 방지)
             time.sleep(random.randint(1, 5))
 
         # 한 사이클 완료 후, 남은 과목이 있다면 추가 대기 (매크로 탐지 방지)
-        if subject_dict:
+        if subject_data:
             random_delay = random.randint(10, 60)
             print(f"\n다음 시도까지 {random_delay}초 대기\n")
             time.sleep(random_delay)
